eb63c9402641a6bd20b91e6d1876125589306fda
zhedlund <zhedlund@student.42berlin.de>
2023-11-20 14:49:12 +0100

    norminette headerfile

--- minitalk.h
+++ minitalk.h
@@ -1,10 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zhedlund <zhedlund@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/20 14:43:03 by zhedlund          #+#    #+#             */
+/*   Updated: 2023/11/20 14:43:44 by zhedlund         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef MINITALK_H
-#define MINITALK_H
+# define MINITALK_H
 
-#include <signal.h>
-#include <unistd.h>
-#include "./libft/libft.h"
+# include <signal.h>
+# include <unistd.h>
+# include "./libft/libft.h"
 
-#define BUFFER 50000
+# define BUFFER 50000
 
 #endif
