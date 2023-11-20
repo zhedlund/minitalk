e10060d21432e0734499060e55d5f1dae7210220
zhedlund <zhedlund@student.42berlin.de>
2023-11-20 14:33:45 +0100

    fixed norminette errors

--- client.c
+++ client.c
@@ -6,57 +6,57 @@
 /*   By: zhedlund <zhedlund@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/17 15:56:00 by zhedlund          #+#    #+#             */
-/*   Updated: 2023/11/19 20:28:25 by zhedlund         ###   ########.fr       */
+/*   Updated: 2023/11/20 14:29:23 by zhedlund         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-void send_bit(pid_t pid, int bit)
+void	send_bit(pid_t pid, int bit)
 {
-    if (bit == 1)
-        kill(pid, SIGUSR1);
-    else
-        kill(pid, SIGUSR2);
-    usleep(42);
+	if (bit == 1)
+		kill(pid, SIGUSR1);
+	else
+		kill(pid, SIGUSR2);
+	usleep(42);
 }
 
-void send_message(pid_t pid, const char *msg)
+void	send_message(pid_t pid, const char *msg)
 {
-    unsigned char	c;
+	unsigned char	c;
 	int				i;
-	
+
 	while (*msg != '\0')
-	{		
-		c = *msg;
-        i = 7;
-        while (i >= 0)
-		{
-            send_bit(pid, (c >> i) & 1);
-            i--;
-        }
-        msg++;
-    }
-    i = 7;
-    while (i >= 0)
 	{
-        send_bit(pid, 0);
-        i--;
-    }
+		c = *msg;
+		i = 7;
+		while (i >= 0)
+		{
+			send_bit(pid, (c >> i) & 1);
+			i--;
+		}
+		msg++;
+	}
+	i = 7;
+	while (i >= 0)
+	{
+		send_bit(pid, 0);
+		i--;
+	}
 }
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-    if (argc != 3)
+	pid_t		server_pid;
+	const char	*message;
+
+	if (argc != 3)
 	{
-        ft_putendl_fd("Usage: <server PID> <message>", 1);
-        exit(1);
-    }
-    pid_t server_pid;
-	const char *message;
-	
+		ft_putendl_fd("Usage: <server PID> <message>", 1);
+		exit(1);
+	}
 	server_pid = atoi(argv[1]);
 	message = argv[2];
-    send_message(server_pid, message);
-    return 0;
+	send_message(server_pid, message);
+	return (0);
 }
--- server.c
+++ server.c
@@ -6,59 +6,58 @@
 /*   By: zhedlund <zhedlund@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/19 17:03:25 by zhedlund          #+#    #+#             */
-/*   Updated: 2023/11/19 20:28:10 by zhedlund         ###   ########.fr       */
+/*   Updated: 2023/11/20 14:32:44 by zhedlund         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #include "minitalk.h"
 
-void signal_handler(int signal)
+void	signal_handler(int signal)
 {
-    static char received_chars[BUFFER];
-    static int bit_count = 0;
-    static int char_index = 0;
+	static char	received_chars[BUFFER];
+	static int	bit_count = 0;
+	static int	char_index = 0;
 
-    if (signal == SIGUSR2)
-        received_chars[char_index] <<= 1;
+	if (signal == SIGUSR2)
+		received_chars[char_index] <<= 1;
 	else if (signal == SIGUSR1)
 	{
-        received_chars[char_index] <<= 1;
-        received_chars[char_index] |= 1;
-    }
+		received_chars[char_index] <<= 1;
+		received_chars[char_index] |= 1;
+	}
 	bit_count++;
-    if (bit_count == 8)
+	if (bit_count == 8)
 	{
-        if (received_chars[char_index] == '\0')
+		if (received_chars[char_index] == '\0')
 		{
-            ft_putendl_fd("\n>>>>> Message received: \n", 1);
+			ft_putendl_fd("\n>>>>> Message received: \n", 1);
 			ft_putendl_fd(received_chars, 1);
-            ft_memset(received_chars, 0, sizeof(received_chars));
+			ft_memset(received_chars, 0, sizeof(received_chars));
 			char_index = 0;
-        }
+		}
 		else
 			char_index++;
-    	bit_count = 0;
+		bit_count = 0;
 	}
 }
 
-int main(void)
+int	main(void)
 {
-    struct sigaction act;
-	
+	struct sigaction	act;
+
 	act.sa_handler = signal_handler;
-    sigemptyset(&act.sa_mask);
+	sigemptyset(&act.sa_mask);
 	act.sa_flags = 0;
-    if (sigaction(SIGUSR1, &act, NULL) < 0
+	if (sigaction(SIGUSR1, &act, NULL) < 0
 		|| sigaction(SIGUSR2, &act, NULL) < 0)
 	{
-        ft_putstr_fd("An error occured", 1);
-        exit(1);
-    }
-    ft_putstr_fd("\nServer running with PID: ", 1);
+		ft_putstr_fd("An error occured", 1);
+		exit(1);
+	}
+	ft_putstr_fd("\nServer running with PID: ", 1);
 	ft_putnbr_fd(getpid(), 1);
 	ft_putchar_fd('\n', 1);
-    while (1)
-        sleep(1);
-    return (0);
+	while (1)
+		sleep(1);
+	return (0);
 }
